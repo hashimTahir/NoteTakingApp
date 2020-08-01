79f2a21924a9b02dfdeb5ad7e68f4274b64c3fa2
hashim <hashimtahir@live.com>
2020-08-02 01:27:34 +0500

    Firebase depen added. All Methods added in repoImpl. LoginInjector complete. project connected with firebase
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/noteapp/di/LoginInjector.kt
+++ app/src/main/java/com/hashim/noteapp/di/LoginInjector.kt
@@ -5,11 +5,17 @@
 package com.hashim.noteapp.di
 
 import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.google.firebase.FirebaseApp
 import com.hashim.noteapp.repository.LoginRepository
 import com.hashim.noteapp.repository.LoginUserRepoImpl
 import com.hashim.noteapp.ui.LoginViewModelProviderFactory
 
-class LoginInjector(application: Application?) {
+class LoginInjector(application: Application) : AndroidViewModel(application) {
+    init {
+        FirebaseApp.initializeApp(application)
+    }
+
     fun hProvidesLoginViewModelFactory(): LoginViewModelProviderFactory {
         return LoginViewModelProviderFactory(hGetLoginRepository())
     }
--- app/src/main/java/com/hashim/noteapp/repository/LoginUserRepoImpl.kt
+++ app/src/main/java/com/hashim/noteapp/repository/LoginUserRepoImpl.kt
@@ -4,6 +4,50 @@
 
 package com.hashim.noteapp.repository
 
-class LoginUserRepoImpl : LoginRepository {
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.hashim.noteapp.models.ResultResponse
+import com.hashim.noteapp.models.ResultResponse.Error.Factory.build
+import com.hashim.noteapp.models.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class LoginUserRepoImpl(
+    val hFireBaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+) : LoginRepository {
+    override suspend fun hGetCurrentUser(): ResultResponse<Exception, User?> {
+        val hFirebaseUser = hFireBaseAuth.currentUser
+
+        return if (hFirebaseUser == null) {
+            ResultResponse.hBuild { null }
+        } else {
+            ResultResponse.hBuild {
+                User(
+                    hFirebaseUser.uid,
+                    hFirebaseUser.displayName ?: ""
+                )
+            }
+        }
+    }
+
+    override suspend fun hSignOutCurrentUser(): ResultResponse<Exception, Unit> {
+        return ResultResponse.hBuild {
+            hFireBaseAuth.signOut()
+        }
+
+    }
+
+    override suspend fun hSignInGoogleUser(idToken: String): ResultResponse<Exception, Unit> {
+        return withContext(Dispatchers.Main) {
+            try {
+                val credential = GoogleAuthProvider.getCredential(idToken, null)
+                hAwaitTaskCompletable(auth.signInWithCredential(credential))
+
+                ResultResponse.hBuild { Unit }
+            } catch (exception: Exception) {
+                ResultResponse.hBuild { throw exception }
+            }
+        }
+    }
 
 }
