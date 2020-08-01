0d8b12e2fa6beaf32ddb606768430cc650cc5e08
hashim <hashimtahir@live.com>
2020-08-01 18:37:10 +0500

    Note detail and note list fragments added.
    Signed-off-by: hashim <hashimtahir@live.com>

---
+++ app/src/main/java/com/hashim/noteapp/ui/notesactivity/fragments/NoteDetialFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020/  8/ 1.  Created by Hashim Tahir
+ */
+
+package com.hashim.noteapp.ui.notesactivity.fragments
+
+import androidx.fragment.app.Fragment
+import com.hashim.noteapp.R
+
+
+class NoteDetialFragment : Fragment(R.layout.fragment_note_detail) {
+
+
+}
