59e2d58d7f414565c60b62e5a34e1dc1afa391fa
hashim <hashimtahir@live.com>
2020-08-02 06:39:53 +0500

    LoginFragment fully complete. Activity result api added to lauch startactivity for result.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/noteapp/ui/loginactivity/loginfragment/LoginFragment.kt
+++ app/src/main/java/com/hashim/noteapp/ui/loginactivity/loginfragment/LoginFragment.kt
@@ -9,19 +9,24 @@ import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.View
 import androidx.activity.addCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.hashim.noteapp.R
 import com.hashim.noteapp.common.Constants
 import com.hashim.noteapp.common.Constants.Companion.H_RC_SIGN_IN
 import com.hashim.noteapp.common.startWithFade
 import com.hashim.noteapp.events.loginevent.LoginEvent
+import com.hashim.noteapp.models.LoginResult
 import com.hashim.noteapp.ui.loginactivity.viewmodel.LoginViewModel
 import com.hashim.noteapp.ui.notesactivity.NotesActivity
 import kotlinx.android.synthetic.main.fragment_login.*
+import timber.log.Timber
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
@@ -94,9 +99,32 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
 
         val hGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), hGoogleSignInOptions)
         val hSignInIntent = hGoogleSignInClient.signInIntent
-        startActivityForResult(hSignInIntent, H_RC_SIGN_IN)
+//        startActivityForResult(hSignInIntent, H_RC_SIGN_IN)
 
+        val hStartActivityForResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                var userToken: String? = null
 
+                try {
+                    val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+
+                    if (account != null) userToken = account.idToken
+                } catch (exception: Exception) {
+                    Timber.d("Login Exceptoin ${exception.toString()}")
+                }
+
+                hLoginViewModel.hHandleEvent(
+                    LoginEvent.OnGoogleSignInResult(
+                        LoginResult(
+                            H_RC_SIGN_IN,
+                            userToken
+                        )
+                    )
+                )
+
+            }
+        hStartActivityForResult.launch(hSignInIntent)
     }
 
 
