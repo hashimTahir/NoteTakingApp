82a1fe7fb48e8a6232ae0ebdc1a111b7aa0d2c3d
hashim <hashimtahir@live.com>
2020-08-02 05:19:45 +0500

    LoginViewModel fully complete.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/noteapp/common/Constants.kt
+++ app/src/main/java/com/hashim/noteapp/common/Constants.kt
@@ -7,6 +7,21 @@ package com.hashim.noteapp.common
 class Constants {
     companion object {
         const val hTag = "hashimTimberTags %s"
+        internal const val H_RC_SIGN_IN = 1337
+        internal const val H_LOGIN_ERROR = "Error retrieving user."
+        internal const val H_SIGN_IN = "SIGN IN"
+        internal const val H_ANTENNA_EMPTY = "ic_antenna_empty"
+        internal const val H_LOADING = "Loading..."
+        internal const val H_ANTENNA_LOOP = "antenna_loop_fast"
+        internal const val H_SIGNED_IN = "Signed In"
+        internal const val H_SIGNED_OUT = "Signed Out"
+        internal const val H_ANTENNA_FULL = "ic_antenna_full"
+        internal const val H_SIGN_OUT = "SIGN OUT"
+
+
+
+
+
 
     }
 }
--- app/src/main/java/com/hashim/noteapp/ui/loginactivity/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/hashim/noteapp/ui/loginactivity/viewmodel/LoginViewModel.kt
@@ -5,6 +5,16 @@
 package com.hashim.noteapp.ui.loginactivity.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import com.hashim.noteapp.common.Constants.Companion.H_ANTENNA_EMPTY
+import com.hashim.noteapp.common.Constants.Companion.H_ANTENNA_FULL
+import com.hashim.noteapp.common.Constants.Companion.H_ANTENNA_LOOP
+import com.hashim.noteapp.common.Constants.Companion.H_LOADING
+import com.hashim.noteapp.common.Constants.Companion.H_LOGIN_ERROR
+import com.hashim.noteapp.common.Constants.Companion.H_RC_SIGN_IN
+import com.hashim.noteapp.common.Constants.Companion.H_SIGNED_IN
+import com.hashim.noteapp.common.Constants.Companion.H_SIGNED_OUT
+import com.hashim.noteapp.common.Constants.Companion.H_SIGN_IN
+import com.hashim.noteapp.common.Constants.Companion.H_SIGN_OUT
 import com.hashim.noteapp.events.loginevent.LoginEvent
 import com.hashim.noteapp.models.LoginResult
 import com.hashim.noteapp.models.ResultResponse
@@ -20,8 +30,17 @@ class LoginViewModel(
     hCoroutineContext: CoroutineContext
 ) : BaseViewModel<LoginEvent<LoginResult>>(hCoroutineContext) {
 
+    //The actual data model is kept private to avoid unwanted tampering
+    private val hUserStateMutableLiveData = MutableLiveData<User?>()
 
-    private val hUserState = MutableLiveData<User?>()
+    //UI Binding
+    internal val hSignInStatusTextMutableLiveData = MutableLiveData<String>()
+    internal val hAuthButtonTextMutableLiveData = MutableLiveData<String>()
+    internal val hSatelliteDrawableMutableLiveData = MutableLiveData<String>()
+
+    //Control Logic
+    internal val hAuthAttemptMutableLiveData = MutableLiveData<Unit>()
+    internal val hStartAnimationMutableLiveData = MutableLiveData<Unit>()
 
 
     init {
@@ -43,7 +62,7 @@ class LoginViewModel(
             var hResultResponse = hLoginRepository.hGetCurrentUser()
             when (hResultResponse) {
                 is ResultResponse.Value -> {
-                    hUserState.value = hResultResponse.value
+                    hUserStateMutableLiveData.value = hResultResponse.value
                     if (hResultResponse.value == null)
                         hShowSignedOutState()
                     else
@@ -55,27 +74,65 @@ class LoginViewModel(
     }
 
     private fun hShowSignedInState() {
-        TODO("Not yet implemented")
+        hSignInStatusTextMutableLiveData.value = H_SIGNED_IN
+        hAuthButtonTextMutableLiveData.value = H_SIGN_OUT
+        hSatelliteDrawableMutableLiveData.value = H_ANTENNA_FULL
     }
 
     private fun hShowSignedOutState() {
-        TODO("Not yet implemented")
+        hSignInStatusTextMutableLiveData.value = H_SIGNED_OUT
+        hAuthButtonTextMutableLiveData.value = H_SIGN_IN
+        hSatelliteDrawableMutableLiveData.value = H_ANTENNA_EMPTY
     }
 
     private fun hShowErrorState() {
-        TODO("Not yet implemented")
+        hSignInStatusTextMutableLiveData.value = H_LOGIN_ERROR
+        hAuthButtonTextMutableLiveData.value = H_SIGN_IN
+        hSatelliteDrawableMutableLiveData.value = H_ANTENNA_EMPTY
+
     }
 
     private fun hOnAuthButtonClick() {
-        TODO("Not yet implemented")
+        if (hUserStateMutableLiveData.value == null)
+            hAuthAttemptMutableLiveData.value = Unit
+        else
+            hSignOutUser()
     }
 
-    private fun hGoogleSignInResult(result: LoginResult) {
-        TODO("Not yet implemented")
+    private fun hSignOutUser() {
+        launch {
+            var hSignOutCurrentUser = hLoginRepository.hSignOutCurrentUser()
+            when (hSignOutCurrentUser) {
+                is ResultResponse.Value -> {
+                    hUserStateMutableLiveData.value = null
+                    hShowSignedOutState()
+                }
+                is ResultResponse.Error -> hShowErrorState()
+            }
+        }
+    }
+
+    private fun hGoogleSignInResult(hResult: LoginResult) {
+        launch {
+            if (hResult.hRequestCode == H_RC_SIGN_IN && hResult.hUserToken != null) {
+                val hCreateGoogleUser = hLoginRepository.hSignInGoogleUser(hResult.hUserToken)
+                if (hCreateGoogleUser is ResultResponse.Value) {
+                    hGetUser()
+                } else {
+                    hShowErrorState()
+                }
+            } else {
+                hShowErrorState()
+            }
+
+        }
     }
 
     private fun hShowLoadingState() {
-        TODO("Not yet implemented")
+        hSignInStatusTextMutableLiveData.value = H_LOADING
+        hSatelliteDrawableMutableLiveData.value = H_ANTENNA_LOOP
+        hStartAnimationMutableLiveData.value = Unit
+
     }
 
 
