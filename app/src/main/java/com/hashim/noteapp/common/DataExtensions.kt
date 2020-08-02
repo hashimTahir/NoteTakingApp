416b8c8bbe5ee0f982f36596960e74ddb0dc8a77
hashim <hashimtahir@live.com>
2020-08-02 23:57:19 +0500

    NotesRepoImpl and data extensions more methods added to convert to and from roomNote->Notes->FirebaseNotes.
    Signed-off-by: hashim <hashimtahir@live.com>

--- app/src/main/java/com/hashim/noteapp/common/DataExtensions.kt
+++ app/src/main/java/com/hashim/noteapp/common/DataExtensions.kt
@@ -5,17 +5,49 @@
 package com.hashim.noteapp.common
 
 import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseUser
+import com.hashim.noteapp.models.Note
+import com.hashim.noteapp.models.User
+import com.hashim.noteapp.room.RoomNote
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
-//Wraps Firebase/GMS calls
-internal suspend fun <T> hAwaitTaskCompletable(task: Task<T>): Unit = suspendCoroutine { continuation ->
-    task.addOnCompleteListener { task ->
-        if (task.isSuccessful) {
-            continuation.resume(Unit)
-        } else {
-            continuation.resumeWithException(task.exception!!)
-        }
+private val RoomNote.toNote: Note
+    get() {
+        return Note(
+            this.creationDate,
+            this.contents,
+            this.upVotes,
+            this.imageUrl,
+            User(this.creatorId)
+        )
+
     }
+
+//Wraps Firebase/GMS calls
+internal suspend fun <T> hAwaitTaskCompletable(task: Task<T>): Unit =
+    suspendCoroutine { continuation ->
+        task.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                continuation.resume(Unit)
+            } else {
+                continuation.resumeWithException(task.exception!!)
+            }
+        }
+
+
+    }
+
+internal fun List<RoomNote>.htoNoteListFromRoomNote(): List<Note> {
+    return this.flatMap {
+        listOf(it.toNote)
+    }
+
 }
+
+internal val FirebaseUser.toUser: User
+    get() = User(
+        uid = this.uid,
+        name = this.displayName ?: ""
+    )
--- app/src/main/java/com/hashim/noteapp/repository/NotesRepoImpl.kt
+++ app/src/main/java/com/hashim/noteapp/repository/NotesRepoImpl.kt
@@ -6,8 +6,11 @@ package com.hashim.noteapp.repository
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.hashim.noteapp.common.htoNoteListFromRoomNote
+import com.hashim.noteapp.common.toUser
 import com.hashim.noteapp.models.Note
 import com.hashim.noteapp.models.ResultResponse
+import com.hashim.noteapp.models.User
 import com.hashim.noteapp.room.NoteDao
 
 class NotesRepoImpl(
@@ -31,15 +34,40 @@ class NotesRepoImpl(
         TODO("Not yet implemented")
     }
 
-    private fun hGetActiveUser(): Any {
-        TODO("Not yet implemented")
+    private fun hGetActiveUser(): User? {
+        return hFirebaseAuth.currentUser?.toUser
     }
 
     override suspend fun hGetAllNotes(): ResultResponse<Exception, List<Note>> {
+        val hUser = hGetActiveUser()
+        return if (hUser != null)
+            hGetRemoteNotes(hUser)
+        else
+            hGetLocalNotes()
+    }
+
+    private suspend fun hGetLocalNotes(): ResultResponse<Exception, List<Note>> {
+        return ResultResponse.hBuild {
+            hNoteDao.getNotes().htoNoteListFromRoomNote()
+        }
+    }
+
+    private fun hGetRemoteNotes(hUser: Any): ResultResponse<Exception, List<Note>> {
         TODO("Not yet implemented")
     }
 
     override suspend fun hDeleteNote(hNote: Note): ResultResponse<Exception, Unit> {
+        val hUser = hGetActiveUser()
+        return if (hUser != null)
+            hDeleteRemoteNote(hNote.copy(hCreator = hUser))
+        else hDeleteLocalNote(hNote)
+    }
+
+    private fun hDeleteLocalNote(hNote: Any): ResultResponse<Exception, Unit> {
+        TODO("Not yet implemented")
+    }
+
+    private fun hDeleteRemoteNote(copy: Note): ResultResponse<Exception, Unit> {
         TODO("Not yet implemented")
     }
 
@@ -47,3 +75,5 @@ class NotesRepoImpl(
         TODO("Not yet implemented")
     }
 }
+
+
